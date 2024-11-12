741bd19393d74df68296668ec7aa949532da8360
Oliver Wang <oliver@Olivers-MBP.ht.home>
2024-11-11 21:09:10 -0500

    chore: load static HTML pages dynamically

--- app/routes/home.py
+++ app/routes/home.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template
 # Create a blueprint
 home_bp = Blueprint('home', __name__)
 
-@home_bp.route('/', methods=['GET'])
-def get_users():
-    return render_template('index.html')
+@home_bp.route('/', defaults={'page': 'index'})
+@home_bp.route('/<page>', methods=['GET'])
+def get_users(page: str):
+    return render_template(f'{page}.html')
