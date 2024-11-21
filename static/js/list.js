4d0ed188e2ab89197b65754044e5eea34951f28d
JOYG <78196817+JoyG2002@users.noreply.github.com>
2024-11-21 17:03:01 -0500

    Front end clean (#18)
    * modify file
    
    * update file, delete unnecessary file
    
    * modify index.html
    
    * save the changes
    
    * Rename app.js to list.js
    
    * modify the javascript for list.html
    
    * small fix for list page
    
    * update css for login and signup html, add log out button

--- static/js/list.js
+++ static/js/list.js
@@ -12,7 +12,9 @@ const renderItem = (items) =>{
         const listItem = document.createElement('li');
 
         const linkElement = document.createElement("a");
+
         linkElement.href = url + item["_id"];
+
         linkElement.textContent = item.name;
         listItem.appendChild(linkElement);
 
@@ -43,45 +45,3 @@ fetch(API_URL)
     itemList.textContent = 'Failed to load user data.';
   });
 
-/*
-axios
-  .get(API_URL)
-  .then(response => {
-
-    const items = response.data;
-    renderItem(items);
-  })
-  .catch(error => {
-    // Report error when get api goes wrong
-    console.error('Error fetching user data:', error);
-    itemList.textContent = 'Failed to load user data.';
-  });
-
-
-          // Sample data for the list items
-        const items = [
-            { name: "Annie Zheng, MSHRM", role: "Talent Acquisition Partner at Bytedance" },
-            { name: "Akhil Vashisht", role: "Delivery Manager @ MindSource" },
-            { name: "Ryan Williams", role: "Technical Recruiter @ Optomi" },
-            { name: "Yi-En (Ivy) Liu", role: "MS in Business Analytics'25 @ UC Irvine" },
-            { name: "Shun Mok Bhark", role: "All things software" },
-            { name: "Samantha Bueno", role: "Manager, Software Engineering at Red Hat" },
-            { name: "Sunil Kumar", role: "Account Manager - Recruitment" },
-            { name: "Yu Chen Hsiao", role: "IM@NCU" }
-        ];
-
-        // Get the <ul> element where we will add the list items
-        const itemList = document.getElementById('users-container');
-
-        // Loop through the items array and create <li> elements
-        items.forEach(item => {
-            // Create a new <li> element
-            const listItem = document.createElement('li');
-            
-            // Set the content of the <li> element
-            listItem.textContent = `${item.name} - ${item.role}`;
-            
-            // Append the <li> element to the <ul> element
-            itemList.appendChild(listItem);
-        });
-*/
