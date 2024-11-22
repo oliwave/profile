8f6fe4e87118dc9a80a9a5ccedcf508120bb0851
JOYG <78196817+JoyG2002@users.noreply.github.com>
2024-11-22 11:19:30 -0500

    Front end clean (#19)
    * modify file
    
    * update file, delete unnecessary file
    
    * modify index.html
    
    * save the changes
    
    * Rename app.js to list.js
    
    * modify the javascript for list.html
    
    * small fix for list page
    
    * update css for login and signup html, add log out button
    
    * add logout function

---
+++ static/js/logout.js
@@ -0,0 +1,18 @@
+const logoutButton = document.getElementById('logout_button');
+
+logoutButton.addEventListener('click', () => {
+    fetch('https://profile-cs633.vercel.app/logout', {
+    //'http://localhost:5001/logout';
+        method: 'GET', // Use GET because your backend requires it
+        credentials: 'include', // Include session cookies if necessary
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log("Logged out successfully");
+            window.location.href = "/login"; // Redirect to login page
+        } else {
+            throw new Error("Logout failed");
+        }
+    })
+    .catch(error => console.error("Error logging out:", error));
+});
